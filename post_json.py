825a9f9d8f61d99bfd0ced884a7a022b964c980d
jetdragon <chdragon9982@gmail.com>
2017-12-07 02:11:31 +0800

    20171207

--- post_json.py
+++ post_json.py
@@ -7,9 +7,9 @@ url = 'http://127.0.0.1:5000/nlp/sentiment'
 headers = {'Content-Type' : 'application/json'}
 r = requests.post(url, data, headers=headers)
 
-result = json.loads(r.content)
-result_str = json.dumps(result)
-result_file = "result.json"
-with open(result_file, "w") as f :
-    f.write(result_str)
-# print(json.loads(r.content))
+# result = json.loads(r.content)
+# result_str = json.dumps(result)
+# result_file = "result.json"
+# with open(result_file, "w") as f :
+#     f.write(result_str)
+print(json.loads(r.content))
